584c0a91fbcadc5f6fa5c43b640a8c6c635212dd
Attila BODIȘ <32338767+attilab97@users.noreply.github.com>
2019-05-15 16:15:36 +0300

    Update train_frcnn.py

--- train_frcnn.py
+++ train_frcnn.py
@@ -6,36 +6,33 @@ import time
 import numpy as np
 from optparse import OptionParser
 import pickle
+import logging
 
 from keras import backend as K
 from keras.optimizers import Adam, SGD, RMSprop
 from keras.layers import Input
 from keras.models import Model
+from keras.utils import generic_utils
 from keras_frcnn import config, data_generators
 from keras_frcnn import losses as losses
-import keras_frcnn.roi_helpers as roi_helpers
-from keras.utils import generic_utils
-import logging
+from keras_frcnn import roi_helpers as roi_helpers
+from keras_frcnn import resnet as nn
+from keras_frcnn.simple_parser import get_data
+#set the logging level
 
 logging.basicConfig(filename = 'logs.log', level = logging.INFO)
 logger = logging.getLogger('keras_frcnn.train_frcnn')
 sys.setrecursionlimit(40000)
 
 parser = OptionParser()
-
 parser.add_option("-p", "--path", dest="train_path", help="Path to training data.")
-parser.add_option("-o", "--parser", dest="parser", help="Parser to use. One of simple or pascal_voc",
-				default="pascal_voc")
 parser.add_option("-n", "--num_rois", type="int", dest="num_rois", help="Number of RoIs to process at once.", default=32)
-parser.add_option("--network", dest="network", help="Base network to use. Supports vgg or resnet50.", default='resnet50')
 parser.add_option("--hf", dest="horizontal_flips", help="Augment with horizontal flips in training. (Default=false).", action="store_true", default=False)
 parser.add_option("--vf", dest="vertical_flips", help="Augment with vertical flips in training. (Default=false).", action="store_true", default=False)
-parser.add_option("--rot", "--rot_90", dest="rot_90", help="Augment with 90 degree rotations in training. (Default=false).",
-				  action="store_true", default=False)
+parser.add_option("--rot", "--rot_90", dest="rot_90", help="Augment with 90 degree rotations in training. (Default=false).",action="store_true", default=False)
 parser.add_option("--num_epochs", type="int", dest="num_epochs", help="Number of epochs.", default=2000)
-parser.add_option("--config_filename", dest="config_filename", help=
-				"Location to store all the metadata related to the training (to be used when testing).",
-				default="config.pickle")
+parser.add_option("--epoch_length", type="int", dest="epoch_length", help="Number of steps per epoch.", default=1000)
+parser.add_option("--config_filename", dest="config_filename", help="Location to store all the metadata related to the training (to be used when testing).",default="config.pickle")
 parser.add_option("--output_weight_path", dest="output_weight_path", help="Output path for weights.", default='./model_frcnn.hdf5')
 parser.add_option("--input_weight_path", dest="input_weight_path", help="Input path for weights. If not specified, will try to load default weights provided by keras.")
 parser.add_option("--log_path", dest="log_path", help="Path of the logs.", default='./logs')
@@ -46,33 +43,16 @@ parser.add_option("--log_path", dest="log_path", help="Path of the logs.", defau
 if not options.train_path:   # if filename is not given
 	parser.error('Error: path to training data must be specified. Pass --path to command line')
 
-if options.parser == 'pascal_voc':
-	from keras_frcnn.pascal_voc_parser import get_data
-elif options.parser == 'simple':
-	from keras_frcnn.simple_parser import get_data
-else:
-	raise ValueError("Command line option parser must be one of 'pascal_voc' or 'simple'")
-
 # pass the settings from the command line, and persist them in the config object
 C = config.Config()
 
+#image augmentation
 C.use_horizontal_flips = bool(options.horizontal_flips)
 C.use_vertical_flips = bool(options.vertical_flips)
 C.rot_90 = bool(options.rot_90)
 
 C.model_path = options.output_weight_path
 C.num_rois = int(options.num_rois)
-
-if options.network == 'vgg':
-	C.network = 'vgg'
-	from keras_frcnn import vgg as nn
-elif options.network == 'resnet50':
-	from keras_frcnn import resnet as nn
-	C.network = 'resnet50'
-else:
-	print('Not a valid model')
-	raise ValueError
-print ('Using ' + C.network + ' network')
 
 # check if weight path was passed via command line
 if options.input_weight_path:
@@ -117,6 +97,7 @@ print('Num val samples {}'.format(len(val_imgs)))
 data_gen_train = data_generators.get_anchor_gt(train_imgs, classes_count, C, nn.get_img_output_length, K.image_dim_ordering(), mode='train')
 data_gen_val = data_generators.get_anchor_gt(val_imgs, classes_count, C, nn.get_img_output_length,K.image_dim_ordering(), mode='val')
 
+#check img_dim_ordering field of keras backend
 if K.image_dim_ordering() == 'th':
 	input_shape_img = (3, None, None)
 else:
@@ -151,14 +132,20 @@ except:
 	print('Could not load pretrained model weights. Weights can be found in the keras application folder \
 		https://github.com/fchollet/keras/tree/master/keras/applications')
 
+#used for rpn
 optimizer = Adam(lr=1e-5)
+
+#used for classifier network
 optimizer_classifier = Adam(lr=1e-5)
+
+#compile both rpn and classifier network
 model_rpn.compile(optimizer=optimizer, loss=[losses.rpn_loss_cls(num_anchors), losses.rpn_loss_regr(num_anchors)])
 model_classifier.compile(optimizer=optimizer_classifier, loss=[losses.class_loss_cls, losses.class_loss_regr(len(classes_count)-1)], metrics={'dense_class_{}'.format(len(classes_count)): 'accuracy'})
+
 #mae = mean absolute error
 model_all.compile(optimizer='sgd', loss='mae', metrics=['accuracy'])
 
-epoch_length = 1000
+epoch_length = int(options.epoch_length)
 num_epochs = int(options.num_epochs)
 iter_num = 0
 
